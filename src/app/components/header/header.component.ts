e211260836a45520c3e9d31ff8f1cfc65f7b9708
kailash-dev <kailash514910@gmail.com>
2025-02-01 14:15:29 +0530

    header comp toggle sidebar related features impl

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,5 +10,8 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  @Output() sidebarToggle = new EventEmitter<void>();
+  toggleSidebar() {
+    this.sidebarToggle.emit();
+  }
 }
